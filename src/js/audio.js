44906fef451e8b5276348a9abc14ddbe0e6e13e4
Niels Christian Konrad Nielsen <niels@rokoko.com>
2018-10-01 23:19:03 +0200

    Objectifying audio sources

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,17 +1,104 @@
 // define variables
-
+// Create audio context and other audio nodes
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var analyser = audioCtx.createAnalyser();
 analyser.fftSize = 256;
-var bufferLength = analyser.frequencyBinCount;
-// console.log(bufferLength);
-var dataArray = new Uint8Array(bufferLength);
 
-var sources = [];
-var panners = [];
+class Sound {
+  constructor(name, path, loop) {
+    this.name = name;
+    this.path = path;
+    this.loop = loop;
+    this.panner = this.createPanner();
+    this.source = this.createSource();
+  }
+  // Create the source
+  createSource () {
+    var source = audioCtx.createBufferSource()
+    var request = new XMLHttpRequest();
+    console.log('../audio/' + this.path);
+    // request.open('GET', '../audio/song-12.2.1_01.mp3', true);
+    request.open('GET', '../audio/' + this.path, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function() {
+      var audioData = request.response;
+      audioCtx.decodeAudioData(audioData, function(buffer) {
+        source.buffer = buffer;
+        // console.log(buffer);
+        if (this.name == 'beat-one') {
+          source.connect(analyser);
+          // source.connect(panners[0]);
+        } else {
+          source.connect(this.panner);
+        }
+        // sources[index].connect(analyser);
+        // sources[index].connect(panner);
+        // panner.connect(audioCtx.destination)
+        source.loop = this.loop;
+        source.start(0);
+      },
+      function(e){ console.log("Error with decoding audio data" + e.err); });
+    }
+    request.send();
+    return source;
+  }
+  // Create it's panner node
+  createPanner () {
+    var panner = audioCtx.createPanner();
+    panner.panningModel = 'HRTF';
+    panner.distanceModel = 'inverse';
+    panner.refDistance = 1;
+    panner.maxDistance = 10000;
+    panner.rolloffFactor = 1;
+    panner.coneInnerAngle = 360;
+    panner.coneOuterAngle = 0;
+    panner.coneOuterGain = 0;
+    panner.connect(audioCtx.destination);
+    return panner;
+  }
+}
 
-panners[0] = createPanner();
-panners[1] = createPanner();
+let sounds = [
+  {name: 'blauw', path: 'tsw-1.0_blauw.wav', isLoop: false},
+  {name: 'braaaum', path: 'tsw-1.0_braaaum.wav', isLoop: true},
+  {name: 'beat-one', path: 'tsw-1.0_beat-1a.wav', isLoop: true}
+];
+
+var source = [];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log(sounds);
+  for (var i = 0; sounds.length; i++) {
+    var elem = sounds[i];
+    source[i] = new Sound(elem.name, elem.path, elem.isLoop);
+    console.log(source[i]);
+  };
+  // source[0] = new Sound('blauw', 'tsw-1.0_blauw.wav', true);
+});
+
+
+
+// Get sphere position
+var sphereElem = document.querySelector('a-sphere');
+// var sphereElem = document.querySelector('a-sphere');
+var cameraElem = document.querySelector('#camera');
+// cameraElem.object3D.updateMatrixWorld();
+// camP = new THREE.Vector3();
+// setTimeout(function () {
+//   camP.setFromMatrixPosition(cameraElem.matrixWorld);
+// }, 4000)
+
+var camElem = document.querySelector("[camera]").getObject3D('camera')
+
+var listener = audioCtx.listener;
+
+function getData(index, src) {
+  
+}
+
+// for (var i = 0; i < sounds.length; i++) {
+//   getData(i, sounds[i]);
+// }
 
 var canvas = document.getElementById('canvas');
 var canvasCtx = canvas.getContext('2d');
@@ -49,177 +136,6 @@ function drawGraph() {
 
 drawGraph();
 
-function createPanner() {
-  var panner = audioCtx.createPanner();
-  panner.panningModel = 'HRTF';
-  panner.distanceModel = 'inverse';
-  panner.refDistance = 1;
-  panner.maxDistance = 10000;
-  panner.rolloffFactor = 1;
-  panner.coneInnerAngle = 360;
-  panner.coneOuterAngle = 0;
-  panner.coneOuterGain = 0;
-  panner.connect(audioCtx.destination);
-  return panner;
-}
-
-// Get sphere position
-var sphereElem = document.querySelector('a-sphere');
-// var sphereElem = document.querySelector('a-sphere');
-var cameraElem = document.querySelector('#camera');
-// cameraElem.object3D.updateMatrixWorld();
-// camP = new THREE.Vector3();
-// setTimeout(function () {
-//   camP.setFromMatrixPosition(cameraElem.matrixWorld);
-// }, 4000)
-
-var sceneEl;
-
-AFRAME.registerComponent('master-scene', {
-  init: function () {
-    sceneEl = this.el;
-    console.log(sceneEl);
-  },
-  testSomethin: function () {
-    console.log('hey');
-  }
-});
-
-// var pairObjectAndSound = function () {
-//   // create an AudioListener and add it to the camera
-//   var listener = new THREE.AudioListener();
-//   camera.add( listener );
-
-//   // create the PositionalAudio object (passing in the listener)
-//   var sound = new THREE.PositionalAudio( listener );
-
-//   // load a sound and set it as the PositionalAudio object's buffer
-//   var audioLoader = new THREE.AudioLoader();
-//   audioLoader.load( '../audio/tsw-1.0_beat-1a.wav', function( buffer ) {
-//     sound.setBuffer( buffer );
-//     sound.setRefDistance( 20 );
-//     sound.play();
-//   });
-
-//   // create an object for the sound to play from
-//   var sphere = new THREE.SphereGeometry( 20, 32, 16 );
-//   var material = new THREE.MeshPhongMaterial( { color: 0xff2200 } );
-//   var mesh = new THREE.Mesh( sphere, material );
-//   sceneEl.add( mesh );
-
-//   // finally add the sound to the mesh
-//   mesh.add( sound );
-// }
-// pairObjectAndSound();
-
-var camElem = document.querySelector("[camera]").getObject3D('camera')
-
-function logSpherePosition () {
-  var spherePos = sphereElem.getAttribute('position');
-  var beatBallPos = beatBall.getAttribute('position');
-  var cameraPos = cameraElem.getAttribute('position');
-  var cameraRot = cameraElem.getAttribute('rotation');
-  if (panners[0].positionX) {
-    panners[0].positionX.value = spherePos.x;
-    panners[0].positionY.value = spherePos.y;
-    panners[0].positionZ.value = spherePos.z;
-  }
-  if (panners[1].positionX) {
-    panners[1].positionX.value = spherePos.x;
-    panners[1].positionY.value = spherePos.y;
-    panners[1].positionZ.value = spherePos.z;
-  }
-  sphereElem.object3D.updateMatrixWorld();
-  var pos = new THREE.Vector3();
-  var rot = new THREE.Quaternion();
-  var scale = new THREE.Vector3();
-  // sphereElem.matrixWorld.decompose(pos, rot, scale);
-  // console.log(pos);
-  // if (panner.orientationX) {
-  //   panner.orientationX.value = 1;
-  //   panner.orientationY.value = 0;
-  //   panner.orientationZ.value = 0;
-  // }
-  if (listener.positionX) {
-    listener.positionX.value = cameraPos.x;
-    listener.positionY.value = cameraPos.y;
-    listener.positionZ.value = cameraPos.z;
-  }
-  listener.setOrientation(cameraRot.x, cameraRot.y, cameraRot.z, camElem.up.x, camElem.up.y, camElem.up.z);
-  // listener.forwardX.setValueAtTime(cameraRot.x, audioCtx.currentTime);
-  // listener.forwardY.setValueAtTime(cameraRot.y, audioCtx.currentTime);
-  // listener.forwardZ.setValueAtTime(cameraRot.z, audioCtx.currentTime);
-  // console.log(cameraPos);
-  // console.log(document.querySelector("[camera]").getObject3D('camera').up.x);
-  window.requestAnimationFrame(logSpherePosition);  
-}
-
-window.requestAnimationFrame(logSpherePosition);
-
-// if(panner.orientationX) {
-//   panner.orientationX.value = 1;
-//   panner.orientationY.value = 0;
-//   panner.orientationZ.value = 0;
-// } else {
-//   panner.setOrientation(1,0,0);
-// }
-
-var listener = audioCtx.listener;
-
-// if (listener.forwardX) {
-//   listener.forwardX.value = 0;
-//   listener.forwardY.value = 0;
-//   listener.forwardZ.value = -1;
-//   listener.upX.value = 0;
-//   listener.upY.value = 1;
-//   listener.upZ.value = 0;
-// } else {
-//   listener.setOrientation(0,0,-1,0,1,0);
-// }
-
-// use XHR to load an audio track, and
-// decodeAudioData to decode it and stick it in a buffer.
-// Then we put the buffer into the source
-
-function getData(index, src) {
-  sources[index] = audioCtx.createBufferSource();
-  var request = new XMLHttpRequest();
-  // request.open('GET', '../audio/song-12.2.1_01.mp3', true);
-  request.open('GET', '../audio/' + src.path, true);
-  request.responseType = 'arraybuffer';
-  request.onload = function() {
-    var audioData = request.response;
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-      sources[index].buffer = buffer;
-      console.log(buffer);
-      if (src.name == 'beat-one') {
-        sources[index].connect(analyser);
-        // sources[index].connect(panners[0]);
-      } else {
-        sources[index].connect(panners[1]);
-      }
-      // sources[index].connect(analyser);
-      // sources[index].connect(panner);
-      // panner.connect(audioCtx.destination)
-      sources[index].loop = src.isLoop;
-      sources[index].start(0);
-    },
-    function(e){ console.log("Error with decoding audio data" + e.err); });
-  }
-  request.send();
-}
-
-var sounds = [
-  {name: 'blauw', path: 'tsw-1.0_blauw.wav', isLoop: false},
-  {name: 'braaaum', path: 'tsw-1.0_braaaum.wav', isLoop: true},
-  {name: 'beat-one', path: 'tsw-1.0_beat-1a.wav', isLoop: true}
-]
-
-for (var i = 0; i < sounds.length; i++) {
-  getData(i, sounds[i]);
-}
-// source.start(0);
-
 // var sound_started = false;
 
 // document.addEventListener('keydown', function (evt) {
@@ -237,95 +153,14 @@ for (var i = 0; i < sounds.length; i++) {
 //   }
 // });
 
-// wire up buttons to stop and play audio
+// var sceneEl;
 
-// play.onclick = function() {
-//   getData();
-//   source.start(0);
-//   play.setAttribute('disabled', 'disabled');
-// }
-
-// stop.onclick = function() {
-//   source.stop(0);
-//   play.removeAttribute('disabled');
-// }
-
-// set up listener and panner position information
-// var WIDTH = window.innerWidth;
-// var HEIGHT = window.innerHeight;
-
-// var xPos = Math.floor(WIDTH/2);
-// var yPos = Math.floor(HEIGHT/2);
-// var zPos = 295;
-
-// // define other variables
-
-// var panner = audioCtx.createPanner();
-// panner.panningModel = 'HRTF';
-// panner.distanceModel = 'inverse';
-// panner.refDistance = 1;
-// panner.maxDistance = 10000;
-// panner.rolloffFactor = 1;
-// panner.coneInnerAngle = 360;
-// panner.coneOuterAngle = 0;
-// panner.coneOuterGain = 0;
-
-// if(panner.orientationX) {
-//   panner.orientationX.setValueAtTime(1, audioCtx.currentTime);
-//   panner.orientationY.setValueAtTime(0, audioCtx.currentTime);
-//   panner.orientationZ.setValueAtTime(0, audioCtx.currentTime);
-// } else {
-//   panner.setOrientation(1,0,0);
-// }
-
-// var listener = audioCtx.listener;
-
-// if(listener.forwardX) {
-//   listener.forwardX.setValueAtTime(0, audioCtx.currentTime);
-//   listener.forwardY.setValueAtTime(0, audioCtx.currentTime);
-//   listener.forwardZ.setValueAtTime(-1, audioCtx.currentTime);
-//   listener.upX.setValueAtTime(0, audioCtx.currentTime);
-//   listener.upY.setValueAtTime(1, audioCtx.currentTime);
-//   listener.upZ.setValueAtTime(0, audioCtx.currentTime);
-// } else {
-//   listener.setOrientation(0,0,-1,0,1,0);
-// }
-
-// var source;
-
-// var play = document.querySelector('.play');
-// var stop = document.querySelector('.stop');
-
-// var boomBox = document.querySelector('.boom-box');
-
-// // var listenerData = document.querySelector('.listener-data');
-// // var pannerData = document.querySelector('.panner-data');
-
-// leftBound = (-xPos) + 50;
-// rightBound = xPos - 50;
-
-// xIterator = WIDTH/150;
-
-// // listener will always be in the same place for this demo
-
-// if(listener.positionX) {
-//   listener.positionX.setValueAtTime(xPos, audioCtx.currentTime);
-//   listener.positionY.setValueAtTime(yPos, audioCtx.currentTime);
-//   listener.positionZ.setValueAtTime(300, audioCtx.currentTime);
-// } else {
-//   listener.setPosition(xPos,yPos,300);
-// }
-
-// // listenerData.innerHTML = 'Listener data: X ' + xPos + ' Y ' + yPos + ' Z ' + 300;
-
-// // panner will move as the boombox graphic moves around on the screen
-// function positionPanner() {
-//   if(panner.positionX) {
-//     panner.positionX.setValueAtTime(xPos, audioCtx.currentTime);
-//     panner.positionY.setValueAtTime(yPos, audioCtx.currentTime);
-//     panner.positionZ.setValueAtTime(zPos, audioCtx.currentTime);
-//   } else {
-//     panner.setPosition(xPos,yPos,zPos);
+// AFRAME.registerComponent('master-scene', {
+//   init: function () {
+//     sceneEl = this.el;
+//     console.log(sceneEl);
+//   },
+//   testSomethin: function () {
+//     console.log('hey');
 //   }
-//   // pannerData.innerHTML = 'Panner data: X ' + xPos + ' Y ' + yPos + ' Z ' + zPos;
-// }
+// });
